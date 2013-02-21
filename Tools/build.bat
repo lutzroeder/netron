0986ecc797e86716cd2f6ee6d5f0b9085628e358
Lutz Roeder <lutzroeder@users.noreply.github.com>
2013-02-21 15:38:57 -0800

    Update libex.ts file name

--- Tools/build.bat
+++ Tools/build.bat
@@ -14,7 +14,7 @@ copy ..\Samples\demo_orgchart.html ..\Build\Release\demo_orgchart.html
 
 echo Building 'Debug\netron.js'
 for %%i in ("..\Source\*.ts") do call set Source=%%Source%% ..\Source\%%i
-node tsc.js -target ES5 -out ..\Build\Debug\netron.js lib.d.ts libex.ts %Source%
+node tsc.js -target ES5 -out ..\Build\Debug\netron.js lib.d.ts libex.d.ts %Source%
 
 echo Building 'Release\netron.js'
 node minify.js ..\Build\Debug\netron.js ..\Build\Release\netron.js
