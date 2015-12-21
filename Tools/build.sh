d4774642300fab2b06dcefc9f4b10f40b797d3e0
Lutz Roeder <lutzroeder@users.noreply.github.com>
2015-12-20 18:51:47 -0800

    Update to TypeScript 1.7.3

--- Tools/build.sh
+++ Tools/build.sh
@@ -7,13 +7,13 @@ mkdir -p ../Build/Debug
 mkdir -p ../Build/Release
 
 echo Building \'Release/*.html\'
-cp ../Samples/demo_orgchart.html ../Build/Debug/demo_orgchart.html
+cp ../Samples/default.html ../Build/Debug/default.html
 
 echo Building \'Release/*.html\'
-cp ../Samples/demo_orgchart.html ../Build/Release/demo_orgchart.html
+cp ../Samples/default.html ../Build/Release/default.html
 
 echo Building \'Debug/netron.js\'
-node tsc.js -target ES5 -out ../Build/Debug/netron.js lib.d.ts libex.d.ts ../Source/*.ts
+node tsc.js -target ES5 -out ../Build/Debug/netron.js ../Library/lib.d.ts ../Library/libex.d.ts ../Source/*.ts
 
 echo Building \'Release/netron.js\'
 node minify.js ../Build/Debug/netron.js ../Build/Release/netron.js
