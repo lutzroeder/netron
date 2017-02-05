89e69933cbab4fa6dea9df20c84508cca3f8a33c
Lutz Roeder <lutzroeder@users.noreply.github.com>
2017-02-04 23:39:20 -0800

    Report errors in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,27 @@ var gulp = require("gulp");
 var sourcemaps = require('gulp-sourcemaps');
 var typescript = require('gulp-typescript');
 var uglify = require('gulp-uglify');
+var open = require('gulp-open');
 
-gulp.task("default", function() {
-    return [
-        gulp.src("./src/*.ts")
-            .pipe(sourcemaps.init())
-            .pipe(typescript({ target: "ES5", out: "netron.js" }))
-            .pipe(uglify())
-            .pipe(sourcemaps.write("."))
-            .pipe(gulp.dest("./dist")),
-        gulp.src([ "./samples/index.html" ])
-            .pipe(gulp.dest("./dist"))
-    ];
+gulp.task("build", [ "build:page", "build:code" ], function() {
+});
+
+gulp.task("build:code", function() {
+    return gulp.src("./src/*.ts")
+        .pipe(sourcemaps.init())
+        .pipe(typescript({ target: "ES5", out: "netron.js" }))
+        .once("error", function() { this.once("finish", () => process.exit(1)) })
+        .pipe(uglify())
+        .pipe(sourcemaps.write("."))
+        .pipe(gulp.dest("./build"));
+});
+
+gulp.task("build:page", function() {
+    return gulp.src([ "./samples/index.html" ])
+        .pipe(gulp.dest("./build"))
+});
+
+gulp.task("default", [ "build" ], function() {
+    return gulp.src("./build/index.html")
+        .pipe(open());
 });
