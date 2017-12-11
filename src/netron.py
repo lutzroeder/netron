1a4ec89d6b6dceda4798df764d688c0b0e36290f
Lutz Roeder <lutzroeder@users.noreply.github.com>
2017-12-10 21:14:08 -0800

    Revert to starting webbrowser using timeout

--- src/netron.py
+++ src/netron.py
@@ -140,8 +140,7 @@ def serve_data(data, file, verbose=False, browse=False, port=8080, host='localho
     server.initialize_data(model, verbose)
     sys.stdout.flush()
     if browse:
-        start_browser = lambda: webbrowser.open(url)
-        threading.Thread(target=start_browser).start()
+        threading.Timer(1, webbrowser.open, args=(url,)).start()
     try:
         server.serve_forever()
     except (KeyboardInterrupt, SystemExit):
