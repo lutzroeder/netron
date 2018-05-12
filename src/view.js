22c144bc7cd0b4145fe0535935034382a3acb17a
Lutz Roeder <lutzroeder@users.noreply.github.com>
2018-05-11 20:24:59 -0700

    Max Zoom 2x (#106)

--- src/view.js
+++ src/view.js
@@ -451,7 +451,7 @@ class View {
     
         // Set up zoom support
         var zoom = d3.zoom();
-        zoom.scaleExtent([0.1, 1]);
+        zoom.scaleExtent([0.1, 2]);
         zoom.on('zoom', (e) => {
             d3.select(outputGroup).attr('transform', d3.event.transform);
         });
