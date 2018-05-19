6b379251acf85ad1c1de4c80c2a3841aed54b0be
Lutz Roeder <lutzroeder@users.noreply.github.com>
2018-05-18 18:20:41 -0700

    Add onnx-converter.py

---
+++ tools/onnx-converter.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+file = sys.argv[1];
+base, extension = os.path.splitext(file)
+
+if extension == '.mlmodel':
+	import coremltools
+	import onnxmltools
+	coreml_model = coremltools.utils.load_spec(file)
+	onnx_model = onnxmltools.convert.convert_coreml(coreml_model)
+	onnxmltools.utils.save_model(onnx_model, base + '.onnx')
+elif extension == '.h5':
+	import keras
+	import onnxmltools
+	keras_model = keras.models.load_model(file)
+	onnx_model = onnxmltools.convert.convert_keras(keras_model)
+	onnxmltools.utils.save_model(onnx_model, base + '.onnx')
