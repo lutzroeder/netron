5cc7ebb7bc19ab91d831a3dba212bfdcd1a1ee86
Lutz Roeder <lutzroeder@users.noreply.github.com>
2018-05-26 19:17:33 -0700

    Add sklearn to coreml-converter

--- tools/coreml-converter.py
+++ tools/coreml-converter.py
@@ -10,3 +10,9 @@ if extension == '.h5':
     import coremltools
     coreml_model = coremltools.converters.keras.convert(file)
     coreml_model.save(base + '.mlmodel')
+elif extension == '.pkl':
+    import coremltools
+    import sklearn
+    sklearn_model = sklearn.externals.joblib.load(file)
+    coreml_model = coremltools.converters.sklearn.convert(sklearn_model)
+    coreml_model.save(base + '.mlmodel')
