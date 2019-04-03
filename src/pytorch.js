ad7e3488df4443c9f51514595654ba374b805641
Lutz Roeder <lutzroeder@users.noreply.github.com>
2019-04-02 20:09:29 -0700

    Update pytorch.js

--- src/pytorch.js
+++ src/pytorch.js
@@ -161,6 +161,7 @@ pytorch.ModelFactory = class {
             constructorTable['torch.nn.modules.upsampling.Upsample'] = function() {};
             constructorTable['torch.nn.modules.upsampling.UpsamplingNearest2d'] = function() {};
             constructorTable['torch.nn.parallel.data_parallel.DataParallel'] = function() {}; 
+            constructorTable['torch.nn.parallel.distributed.DistributedDataParallel'] = function() {};
             constructorTable['torch.nn.parameter.Parameter'] = function(data, requires_grad) {
                 this.data = data; this.requires_grad = requires_grad;
             };
