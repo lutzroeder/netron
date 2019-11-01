ce078ccc2c0952867323b3c851ad5f1844691690
Tee Jung <tee.ty.jung@openedges.com>
2019-11-01 17:25:06 +0900

    add script to build armnn-schema.js

---
+++ tools/armnn-script.js
@@ -0,0 +1,17 @@
+var flatbuffers = require('../third_party/flatbuffers/js/flatbuffers.js').flatbuffers;
+var schema = require('../src/armnn-schema.js.new');
+
+console.log('');
+console.log('armnnSerializer.castLayer = function(schema, layer) {');
+console.log('    let layerType = layer.layerType();');
+
+for (k of Object.keys(schema.armnnSerializer.Layer)) {
+    if (k == "NONE")
+        continue;
+
+    console.log("    if (layerType == schema.Layer." + k + ")");
+    console.log("        return layer.layer(new schema." + k + ");");
+}
+
+console.log('    return null;');
+console.log('}')
