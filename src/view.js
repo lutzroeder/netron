6f1abdfdcb0a191df48402ec87678231a5309b62
Lutz Roeder <lutzroeder@users.noreply.github.com>
2020-06-20 11:59:54 -0700

    UFF .trt detection (#511)

--- src/view.js
+++ src/view.js
@@ -1140,7 +1140,7 @@ view.ModelFactoryService = class {
         this.register('./tflite', [ '.tflite', '.lite', '.tfl', '.bin', '.pb', '.tmfile', '.h5', '.model', '.json' ]);
         this.register('./tf', [ '.pb', '.meta', '.pbtxt', '.prototxt', '.json', '.index', '.ckpt' ]);
         this.register('./mediapipe', [ '.pbtxt' ]);
-        this.register('./uff', [ '.uff', '.pb', '.pbtxt', '.uff.txt' ]);
+        this.register('./uff', [ '.uff', '.pb', '.trt', '.pbtxt', '.uff.txt' ]);
         this.register('./sklearn', [ '.pkl', '.pickle', '.joblib', '.model', '.meta', '.pb', '.pt', '.h5' ]);
         this.register('./cntk', [ '.model', '.cntk', '.cmf', '.dnn' ]);
         this.register('./paddle', [ '.paddle', '__model__' ]);
