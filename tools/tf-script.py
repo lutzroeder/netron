c8f89316b79ea022d14aec8f4c0d05aa426b0429
Lutz Roeder <lutzroeder@users.noreply.github.com>
2020-07-02 18:13:04 -0700

    Update tf script

--- tools/tf-script.py
+++ tools/tf-script.py
@@ -260,7 +260,7 @@ def metadata():
             return 'false'
         raise Exception()
 
-    tensorflow_repo_dir = os.path.join(os.path.dirname(__file__), '../third_party/src/tensorflow')
+    tensorflow_repo_dir = os.path.join(os.path.dirname(__file__), '../third_party/src/tf')
     api_def_map = read_api_def_map(os.path.join(tensorflow_repo_dir, 'tensorflow/core/api_def/base_api'))
     input_file = os.path.join(tensorflow_repo_dir, 'tensorflow/core/ops/ops.pbtxt')
     ops_list = op_def_pb2.OpList()
