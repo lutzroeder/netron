729c63d6ec1e1564cd95fa1917db36aab9365a29
Lutz Roeder <lutzroeder@users.noreply.github.com>
2020-11-01 00:21:33 -0700

    Update pytorch.js

--- source/pytorch.js
+++ source/pytorch.js
@@ -1144,6 +1144,7 @@ pytorch.Execution = class {
         this._registerConstructor('torch.optim.lr_scheduler.LambdaLR', function() {});
         this._registerConstructor('torch.optim.lr_scheduler.MultiStepLR', function() {});
         this._registerConstructor('torch.optim.lr_scheduler.StepLR', function() {});
+        this._registerConstructor('torch.optim.optimizer._RequiredParameter', function() {});
         this._registerConstructor('torch.optim.rmsprop.RMSprop', function() {});
         this._registerConstructor('torch.optim.sgd.SGD', function() {});
         this._registerConstructor('torch.quantization.observer._PartialWrapper', function() {});
