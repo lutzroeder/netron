63b9febf8e6b7f0c8c1b18906d7c3e31f2668e75
Lutz Roeder <lutzroeder@users.noreply.github.com>
2021-01-06 19:15:03 -0800

    Update mnn-schema.js

--- source/mnn-schema.js
+++ source/mnn-schema.js
@@ -130,6 +130,14 @@ $root.MNN.OpType = {
     ArgMin: 130,
     LinSpace: 131,
     RandomUniform: 132,
+    TensorArray: 133,
+    TensorArraySize: 134,
+    TensorArrayRead: 135,
+    TensorArrayWrite: 136,
+    TensorArrayGather: 137,
+    TensorArrayScatter: 138,
+    TensorArraySplit: 139,
+    TensorArrayConcat: 140,
     Plugin: 256,
     Select: 257,
     ZerosLike: 258,
@@ -303,6 +311,7 @@ $root.MNN.OpParameter = class {
             case 86: return $root.MNN.IfParam.decode(reader, position);
             case 87: return $root.MNN.RandomUniform.decode(reader, position);
             case 88: return $root.MNN.LayerNorm.decode(reader, position);
+            case 89: return $root.MNN.TensorArray.decode(reader, position);
         }
         return undefined;
     }
@@ -397,6 +406,7 @@ $root.MNN.OpParameter = class {
             case 'IfParam': return $root.MNN.IfParam.decodeText(reader, json);
             case 'RandomUniform': return $root.MNN.RandomUniform.decodeText(reader, json);
             case 'LayerNorm': return $root.MNN.LayerNorm.decodeText(reader, json);
+            case 'TensorArray': return $root.MNN.TensorArray.decodeText(reader, json);
         }
         return undefined;
     }
@@ -568,7 +578,8 @@ $root.MNN.IDSTQuan = class IDSTQuan {
 
 $root.MNN.QuantizeAlgo = {
     DEFAULT: 0,
-    OVERFLOW_AWARE: 1
+    OVERFLOW_AWARE: 1,
+    WINOGRAD_AWARE: 2
 };
 
 $root.MNN.QuantizedFloatParam = class QuantizedFloatParam {
@@ -581,6 +592,10 @@ $root.MNN.QuantizedFloatParam = class QuantizedFloatParam {
         $.tensorScale = reader.typedArray(position, 10, Float32Array);
         $.method = reader.int8_(position, 12, 0);
         $.nbits = reader.int32_(position, 14, 8);
+        $.zeroPoint = reader.int8_(position, 16, 0);
+        $.outputZeroPoint = reader.int8_(position, 18, 0);
+        $.clampMin = reader.int8_(position, 20, -128);
+        $.clampMax = reader.int8_(position, 22, 127);
         return $;
     }
 };
@@ -1553,6 +1568,18 @@ $root.MNN.RandomUniform = class RandomUniform {
         $.seed2 = reader.int32_(position, 6, 0);
         $.type = reader.int32_(position, 8, 1);
         $.T = reader.int32_(position, 10, 3);
+        return $;
+    }
+};
+
+$root.MNN.TensorArray = class TensorArray {
+
+    static decode(reader, position) {
+        const $ = new $root.MNN.TensorArray();
+        $.dynamic_size = reader.bool_(position, 4, false);
+        $.identical_element_shapes = reader.bool_(position, 6, false);
+        $.element_shape = reader.typedArray(position, 8, Int32Array);
+        $.T = reader.int32_(position, 10, 1);
         return $;
     }
 };
