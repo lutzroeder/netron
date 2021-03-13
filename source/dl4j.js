1ea0d11208f9ddbffd0e32e87048cd7a8adcba09
Lutz Roeder <lutzroeder@users.noreply.github.com>
2021-03-13 12:36:42 -0800

    Update dl4j-metadata.json

--- source/dl4j.js
+++ source/dl4j.js
@@ -466,23 +466,16 @@ dl4j.Metadata = class {
     }
 
     constructor(data) {
-        this._map = {};
+        this._map = new Map();
         this._attributeCache = {};
         if (data) {
-            if (data) {
-                const items = JSON.parse(data);
-                if (items) {
-                    for (const item of items) {
-                        item.schema.name = item.name;
-                        this._map[item.name] = item.schema;
-                    }
-                }
-            }
+            const metadata = JSON.parse(data);
+            this._map = new Map(metadata.map((item) => [ item.name, item ]));
         }
     }
 
     type(name) {
-        return this._map[name];
+        return this._map.get(name);
     }
 
     attribute(type, name) {
