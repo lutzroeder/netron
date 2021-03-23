63387a2beb4debb10d0a60effc8f82871e3113de
Lutz Roeder <lutzroeder@users.noreply.github.com>
2021-03-22 21:19:56 -0700

    Update python.js

--- source/python.js
+++ source/python.js
@@ -1944,6 +1944,7 @@ python.Execution = class {
         this.registerType('sklearn.linear_model._bayes.BayesianRidge', class {});
         this.registerType('sklearn.linear_model._coordinate_descent.ElasticNet', class {});
         this.registerType('sklearn.linear_model._logistic.LogisticRegression', class {});
+        this.registerType('sklearn.linear_model._ridge.Ridge', class {});
         this.registerType('sklearn.linear_model._sgd_fast.Hinge', class {});
         this.registerType('sklearn.linear_model._sgd_fast.ModifiedHuber', class {});
         this.registerType('sklearn.linear_model._sgd_fast.SquaredHinge', class {});
