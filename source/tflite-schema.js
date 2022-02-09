ac267da277bcb70bd6dc42d0193fe691414101a0
Lutz Roeder <lutzroeder@users.noreply.github.com>
2022-02-08 21:14:52 -0800

    Update tflite-schema.js

--- source/tflite-schema.js
+++ source/tflite-schema.js
@@ -369,7 +369,8 @@ $root.tflite.BuiltinOperator = {
     BUCKETIZE: 147,
     RANDOM_UNIFORM: 148,
     MULTINOMIAL: 149,
-    GELU: 150
+    GELU: 150,
+    DYNAMIC_UPDATE_SLICE: 151
 };
 
 $root.tflite.BuiltinOptions = class {
@@ -492,6 +493,7 @@ $root.tflite.BuiltinOptions = class {
             case 114: return $root.tflite.RandomOptions.decode(reader, position);
             case 115: return $root.tflite.BucketizeOptions.decode(reader, position);
             case 116: return $root.tflite.GeluOptions.decode(reader, position);
+            case 117: return $root.tflite.DynamicUpdateSliceOptions.decode(reader, position);
         }
         return undefined;
     }
@@ -614,6 +616,7 @@ $root.tflite.BuiltinOptions = class {
             case 'RandomOptions': return $root.tflite.RandomOptions.decodeText(reader, json);
             case 'BucketizeOptions': return $root.tflite.BucketizeOptions.decodeText(reader, json);
             case 'GeluOptions': return $root.tflite.GeluOptions.decodeText(reader, json);
+            case 'DynamicUpdateSliceOptions': return $root.tflite.DynamicUpdateSliceOptions.decodeText(reader, json);
         }
         return undefined;
     }
@@ -2452,6 +2455,19 @@ $root.tflite.GeluOptions = class GeluOptions {
     static decodeText(reader, json) {
         const $ = new $root.tflite.GeluOptions();
         $.approximate = reader.value(json.approximate, false);
+        return $;
+    }
+};
+
+$root.tflite.DynamicUpdateSliceOptions = class DynamicUpdateSliceOptions {
+
+    static decode(/* reader, position */) {
+        const $ = new $root.tflite.DynamicUpdateSliceOptions();
+        return $;
+    }
+
+    static decodeText(/* reader, json */) {
+        const $ = new $root.tflite.DynamicUpdateSliceOptions();
         return $;
     }
 };
