633493e467fa7a36c2fc7dcbeb0b979061296a18
Lutz Roeder <lutzroeder@users.noreply.github.com>
2022-03-31 20:07:37 -0700

    TensorFlow .txt detection

--- source/tf.js
+++ source/tf.js
@@ -12,7 +12,7 @@ tf.ModelFactory = class {
     match(context) {
         const identifier = context.identifier;
         const extension = identifier.split('.').pop().toLowerCase();
-        if (extension === 'pbtxt' || extension === 'prototxt' || extension === 'pt') {
+        if (extension === 'pbtxt' || extension === 'prototxt' || extension === 'pt' || extension === 'txt') {
             if (identifier.endsWith('predict_net.pbtxt') || identifier.endsWith('predict_net.prototxt') ||
                 identifier.endsWith('init_net.pbtxt') || identifier.endsWith('init_net.prototxt')) {
                 return undefined;
