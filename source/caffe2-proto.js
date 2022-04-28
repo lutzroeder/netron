93dc7436ddda4bbb27d37e34ad066f827d87d704
Lutz Roeder <lutzroeder@users.noreply.github.com>
2022-04-27 18:08:42 -0700

    Update caffe2-proto.js

--- source/caffe2-proto.js
+++ source/caffe2-proto.js
@@ -854,7 +854,7 @@ $root.caffe2.DeviceTypeProto = {
     "PROTO_FPGA": 7,
     "PROTO_ORT": 8,
     "PROTO_XLA": 9,
-    "PROTO_MLC": 10,
+    "PROTO_MPS": 10,
     "PROTO_COMPILE_TIME_MAX_DEVICE_TYPES": 11
 };
 
