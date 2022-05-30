1e1bd70cb58bfe77543cf855f13fc992438a7585
Lutz Roeder <lutzroeder@users.noreply.github.com>
2022-05-30 08:06:43 -0700

    Update PyTorch script

--- tools/pytorch-script.py
+++ tools/pytorch-script.py
@@ -36,15 +36,6 @@ def metadata():
                 raise Exception('\'' + class_name + '\' missing __doc__.')
             # print(docstring)
 
-    # with io.open(json_file, 'w', newline='') as fout:
-    #     json_data = json.dumps(json_root, sort_keys=True, indent=2)
-    #     for line in json_data.splitlines():
-    #         line = line.rstrip()
-    #         if sys.version_info[0] < 3:
-    #             line = unicode(line)
-    #         fout.write(line)
-    #         fout.write('\n')
-
 if __name__ == '__main__':
     command_table = { 'metadata': metadata }
     command = sys.argv[1]
