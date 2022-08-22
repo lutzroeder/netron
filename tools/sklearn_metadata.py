6f80831706cef1b6ef25c0671730faf294266abd
Lutz Roeder <lutzroeder@users.noreply.github.com>
2022-08-22 23:13:53 +0100

    Update scikit-learn script

--- tools/sklearn_metadata.py
+++ tools/sklearn_metadata.py
@@ -4,7 +4,6 @@ import json
 import os
 import pydoc
 import re
-import sys
 
 def _split_docstring(value):
     headers = {}
@@ -32,11 +31,11 @@ def _update_description(schema, lines):
         schema['description'] = '\n'.join(lines)
 
 def _attribute_value(attribute_type, attribute_value):
+    if attribute_value in ('None', 'np.finfo(float).eps'):
+        return None
     if attribute_type in ('float32', 'int32', 'boolean', 'string'):
         if attribute_value in ("'auto'", '"auto"') or attribute_type == 'string':
             return attribute_value.strip("'").strip('"')
-    if attribute_type in (None, 'float32', 'int32') and attribute_value == 'None':
-        return None
     if attribute_type == 'float32':
         return float(attribute_value)
     if attribute_type == 'int32':
@@ -60,159 +59,34 @@ def _find_attribute(schema, name):
 def _update_attributes(schema, lines):
     while len(lines) > 0:
         line = lines.pop(0)
-        line = re.sub(r',\s+', ', ', line)
-        line = line.rstrip('.')
-        name, line = line.split(':', 1)
-        if not line:
+        match = re.match(r'\s*(\w*)\s*:\s*(.*)\s*', line)
+        if not match:
             raise Exception("Expected ':' in parameter.")
-        name = name.strip(' ')
-        line = line.strip(' ')
-        attribute_type = None
+        name = match.group(1)
+        line = match.group(2)
+        attribute = _find_attribute(schema, name)
+        match = re.match(r'(.*),\s*default=(.*)\s*', line)
+        default_value = None
+        if match:
+            line = match.group(1)
+            default_value = match.group(2)
         attribute_types = {
             'float': 'float32',
             'boolean': 'boolean',
             'bool': 'boolean',
+            'str': 'string',
             'string': 'string',
             'int': 'int32',
             'integer': 'int32'
         }
-        skip_map = {
-            "'sigmoid' or 'isotonic'",
-            'instance BaseEstimator',
-            'callable or None (default)',
-            'str or callable',
-            "string {'english'}, list, or None (default)",
-            'tuple (min_n, max_n)',
-            "string, {'word', 'char', 'char_wb'} or callable",
-            "{'word', 'char'} or callable",
-            "string, {'word', 'char'} or callable",
-            'int, float, None or string',
-            "int, float, None or str",
-            "int or None, optional (default=None)",
-            "'l1', 'l2' or None, optional",
-            "{'strict', 'ignore', 'replace'} (default='strict')",
-            "{'ascii', 'unicode', None} (default=None)",
-            "string {'english'}, list, or None (default=None)",
-            "tuple (min_n, max_n) (default=(1, 1))",
-            "float in range [0.0, 1.0] or int (default=1.0)",
-            "float in range [0.0, 1.0] or int (default=1)",
-            "'l1', 'l2' or None, optional (default='l2')",
-            "str {'auto', 'full', 'arpack', 'randomized'}",
-            "str {'filename', 'file', 'content'}",
-            "str, {'word', 'char', 'char_wb'} or callable",
-            "str {'english'}, list, or None (default=None)",
-            "{'scale', 'auto'} or float, optional (default='scale')",
-            "{'word', 'char', 'char_wb'} or callable, default='word'",
-            "{'scale', 'auto'} or float, default='scale'",
-            "{'uniform', 'distance'} or callable, default='uniform'",
-            "int, RandomState instance or None (default)",
-            "list of (string, transformer) tuples",
-            "list of tuples",
-            "{'drop', 'passthrough'} or estimator, default='drop'",
-            "'auto' or a list of array-like, default='auto'",
-            "callable",
-            "int or \"all\", optional, default=10",
-            "number, string, np.nan (default) or None",
-            "estimator object",
-            "dict or list of dictionaries",
-            "int, or str, default=n_jobs",
-            "'raise' or numeric, default=np.nan",
-            "'auto' or float, default=None",
-            "float, default=np.finfo(float).eps",
-            "int, float, str, np.nan or None, default=np.nan",
-            "list of (str, transformer) tuples",
-            "int, float, str, np.nan, None or pandas.NA, default=np.nan",
-            "{'first', 'if_binary'} or an array-like of shape (n_features,), default=None",
-            "{'first', 'if_binary'} or a array-like of shape (n_features,), default=None",
-            "{'linear', 'poly', 'rbf', 'sigmoid', 'precomputed'} or callable, default='rbf'",
-            "estimator instance",
-            "{'ascii', 'unicode'} or callable, default=None",
-            "{'l1', 'l2'} or None, default='l2'"
-        }
-        if line == 'str':
-            line = 'string'
-        if line in skip_map:
-            line = ''
-        elif line.startswith('{'):
-            if line.endswith('}'):
-                line = ''
-            else:
-                end = line.find('},')
-                if end == -1:
-                    raise Exception("Expected '}' in parameter.")
-                line = line[end + 2:].strip(' ')
-        elif line.startswith("'"):
-            while line.startswith("'"):
-                end = line.find("',")
-                if end == -1:
-                    raise Exception("Expected \' in parameter.")
-                line = line[end + 2:].strip(' ')
-        elif line in attribute_types:
-            attribute_type = line
-            line = ''
-        elif line.startswith('int, RandomState instance or None,'):
-            line = line[len('int, RandomState instance or None,'):]
-        elif line.startswith('int, or str, '):
-            line = line[len('int, or str, '):]
-        elif line.find('|') != -1:
-            line = ''
-        else:
-            space = line.find(' {')
-            if space != -1 and line[0:space] in attribute_types and line[space:].find('}') != -1:
-                attribute_type = line[0:space]
-                end = line[space:].find('}')
-                line = line[space+end+1:]
-            else:
-                comma = line.find(',')
-                if comma == -1:
-                    comma = line.find(' (')
-                    if comma == -1:
-                        raise Exception("Expected ',' in parameter.")
-                attribute_type = line[0:comma]
-                line = line[comma + 1:].strip(' ')
-        attribute_type = attribute_types.get(attribute_type, None)
-        optional = False
-        default = None
-        while len(line.strip(' ')) > 0:
-            line = line.strip(' ')
-            if line.startswith('optional ') or line.startswith('optional,'):
-                optional = True
-                line = line[9:]
-            elif line.startswith('optional'):
-                optional = True
-                line = ''
-            elif line.startswith('('):
-                close = line.index(')')
-                if close == -1:
-                    raise Exception("Expected ')' in parameter.")
-                line = line[1:close]
-            elif line.endswith(' by default'):
-                default = line[0:-11]
-                line = ''
-            elif line.startswith('default =') or line.startswith('default :'):
-                default = line[9:].strip(' ')
-                line = ''
-            elif line.startswith('default ') or \
-                line.startswith('default=') or line.startswith('default:'):
-                default = line[8:].strip(' ')
-                line = ''
-            else:
-                comma = line.index(',')
-                if comma == -1:
-                    raise Exception("Expected ',' in parameter.")
-                line = line[comma+1:]
+        attribute_type = attribute_types.get(line, None)
+        if default_value:
+            attribute['default'] = _attribute_value(attribute_type, default_value)
         description = []
         while len(lines) > 0 and (len(lines[0].strip(' ')) == 0 or lines[0].startswith('        ')):
             line = lines.pop(0).lstrip(' ')
             description.append(line)
-        attribute = _find_attribute(schema, name)
         attribute['description'] = '\n'.join(description)
-        if attribute_type:
-            attribute['type'] = attribute_type
-        if optional:
-            attribute['optional'] = True
-        if default:
-            attribute['default'] = _attribute_value(attribute_type, default)
 
 def _metadata():
     json_file = os.path.join(os.path.dirname(__file__), '../source/sklearn-metadata.json')
