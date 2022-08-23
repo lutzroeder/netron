e3d02f423a95438e426813630600080e22b87217
Lutz Roeder <lutzroeder@users.noreply.github.com>
2022-08-23 17:13:44 +0100

    Update ONNX script

--- tools/onnx_metadata.py
+++ tools/onnx_metadata.py
@@ -5,6 +5,16 @@ import json
 import os
 import re
 import sys
+import onnx.backend.test.case
+import onnx.defs
+
+def _read(path):
+    with open(path, 'r', encoding='utf-8') as file:
+        return file.read()
+
+def _write(path, content):
+    with open(path, 'w', encoding='utf-8') as file:
+        file.write(content)
 
 categories = {
     'Constant': 'Constant',
@@ -109,10 +119,76 @@ def _format_description(description):
     description = re.sub("\\[(.+)\\]\\(([^ ]+?)( \"(.+)\")?\\)", replace_line, description)
     return description
 
+def _update_attributes(json_schema, schema):
+    json_schema['attributes'] = []
+    for _ in collections.OrderedDict(schema.attributes.items()).values():
+        json_attribute = {}
+        json_attribute['name'] = _.name
+        attribute_type = _get_attr_type(_.type, _.name, schema.name, schema.domain)
+        if attribute_type:
+            json_attribute['type'] = attribute_type
+        elif 'type' in json_attribute:
+            del json_attribute['type']
+        json_attribute['required'] = _.required
+        default_value = _get_attr_default_value(_.default_value)
+        if default_value:
+            json_attribute['default'] = default_value
+        json_attribute['description'] = _format_description(_.description)
+        json_schema['attributes'].append(json_attribute)
+
+def _update_inputs(json_schema, inputs):
+    json_schema['inputs'] = []
+    for _ in inputs:
+        json_input = {}
+        json_input['name'] = _.name
+        json_input['type'] = _.typeStr
+        if _.option == onnx.defs.OpSchema.FormalParameterOption.Optional:
+            json_input['option'] = 'optional'
+        elif _.option == onnx.defs.OpSchema.FormalParameterOption.Variadic:
+            json_input['list'] = True
+        json_input['description'] = _format_description(_.description)
+        json_schema['inputs'].append(json_input)
+
+def _update_outputs(json_schema, outputs):
+    json_schema['outputs'] = []
+    for _ in outputs:
+        json_output = {}
+        json_output['name'] = _.name
+        json_output['type'] = _.typeStr
+        if _.option == onnx.defs.OpSchema.FormalParameterOption.Optional:
+            json_output['option'] = 'optional'
+        elif _.option == onnx.defs.OpSchema.FormalParameterOption.Variadic:
+            json_output['list'] = True
+        json_output['description'] = _format_description(_.description)
+        json_schema['outputs'].append(json_output)
+
+def _update_type_constraints(json_schema, type_constraints):
+    json_schema['type_constraints'] = []
+    for _ in type_constraints:
+        json_schema['type_constraints'].append({
+            'description': _.description,
+            'type_param_str': _.type_param_str,
+            'allowed_type_strs': _.allowed_type_strs
+        })
+
+def _update_snippets(json_schema, snippets):
+    json_schema['examples'] = []
+    for summary, code in sorted(snippets):
+        lines = code.splitlines()
+        while len(lines) > 0 and re.search("\\s*#", lines[-1]):
+            lines.pop()
+            if len(lines) > 0 and len(lines[-1]) == 0:
+                lines.pop()
+        json_schema['examples'].append({
+            'summary': summary,
+            'code': '\n'.join(lines)
+        })
+
+def _format_range(value):
+    return '&#8734;' if value == 2147483647 else str(value)
+
 def _metadata():
     json_root = []
-    import onnx.backend.test.case # pylint: disable=import-outside-toplevel
-    import onnx.defs # pylint: disable=import-outside-toplevel
     snippets = onnx.backend.test.case.collect_snippets()
     all_schemas_with_history = onnx.defs.get_all_schemas_with_history()
     for schema in all_schemas_with_history:
@@ -121,110 +197,44 @@ def _metadata():
         json_schema['module'] = schema.domain if schema.domain else 'ai.onnx'
         json_schema['version'] = schema.since_version
         json_schema['support_level'] = _generate_json_support_level_name(schema.support_level)
-        if schema.doc:
-            json_schema['description'] = _format_description(schema.doc.lstrip())
+        json_schema['description'] = _format_description(schema.doc.lstrip())
         if schema.attributes:
-            json_schema['attributes'] = []
-            for _ in collections.OrderedDict(schema.attributes.items()).values():
-                json_attribute = {}
-                json_attribute['name'] = _.name
-                attribute_type = _get_attr_type(_.type, _.name, schema.name, schema.domain)
-                if attribute_type:
-                    json_attribute['type'] = attribute_type
-                elif 'type' in json_attribute:
-                    del json_attribute['type']
-                json_attribute['required'] = _.required
-                default_value = _get_attr_default_value(_.default_value)
-                if default_value:
-                    json_attribute['default'] = default_value
-                json_attribute['description'] = _format_description(_.description)
-                json_schema['attributes'].append(json_attribute)
+            _update_attributes(json_schema, schema)
         if schema.inputs:
-            json_schema['inputs'] = []
-            for _ in schema.inputs:
-                json_input = {}
-                json_input['name'] = _.name
-                json_input['type'] = _.typeStr
-                if _.option == onnx.defs.OpSchema.FormalParameterOption.Optional:
-                    json_input['option'] = 'optional'
-                elif _.option == onnx.defs.OpSchema.FormalParameterOption.Variadic:
-                    json_input['list'] = True
-                json_input['description'] = _format_description(_.description)
-                json_schema['inputs'].append(json_input)
+            _update_inputs(json_schema, schema.inputs)
         json_schema['min_input'] = schema.min_input
         json_schema['max_input'] = schema.max_input
         if schema.outputs:
-            json_schema['outputs'] = []
-            for _ in schema.outputs:
-                json_output = {}
-                json_output['name'] = _.name
-                json_output['type'] = _.typeStr
-                if _.option == onnx.defs.OpSchema.FormalParameterOption.Optional:
-                    json_output['option'] = 'optional'
-                elif _.option == onnx.defs.OpSchema.FormalParameterOption.Variadic:
-                    json_output['list'] = True
-                json_output['description'] = _format_description(_.description)
-                json_schema['outputs'].append(json_output)
+            _update_outputs(json_schema, schema.outputs)
         json_schema['min_output'] = schema.min_output
         json_schema['max_output'] = schema.max_output
-        def format_range(value):
-            return '&#8734;' if value == 2147483647 else str(value)
         if schema.min_input != schema.max_input:
-            json_schema['inputs_range'] = format_range(schema.min_input) + ' - ' \
-                + format_range(schema.max_input)
+            json_schema['inputs_range'] = _format_range(schema.min_input) + ' - ' \
+                + _format_range(schema.max_input)
         if schema.min_output != schema.max_output:
-            json_schema['outputs_range'] = format_range(schema.min_output) + ' - ' \
-                + format_range(schema.max_output)
+            json_schema['outputs_range'] = _format_range(schema.min_output) + ' - ' \
+                + _format_range(schema.max_output)
         if schema.type_constraints:
-            json_schema['type_constraints'] = []
-            for type_constraint in schema.type_constraints:
-                json_schema['type_constraints'].append({
-                    'description': type_constraint.description,
-                    'type_param_str': type_constraint.type_param_str,
-                    'allowed_type_strs': type_constraint.allowed_type_strs
-                })
+            _update_type_constraints(json_schema, schema.type_constraints)
         if schema.name in snippets:
-            def update_code(code):
-                lines = code.splitlines()
-                while len(lines) > 0 and re.search("\\s*#", lines[-1]):
-                    lines.pop()
-                    if len(lines) > 0 and len(lines[-1]) == 0:
-                        lines.pop()
-                return '\n'.join(lines)
-            json_schema['examples'] = []
-            for summary, code in sorted(snippets[schema.name]):
-                json_schema['examples'].append({
-                    'summary': summary,
-                    'code': update_code(code)
-                })
+            _update_snippets(json_schema, snippets[schema.name])
         if schema.name in categories:
             json_schema['category'] = categories[schema.name]
         json_root.append(json_schema)
     json_root = sorted(json_root, key=lambda item: item['name'] + ':' + \
         str(item['version'] if 'version' in item else 0).zfill(4))
-    json_file = os.path.join(os.path.dirname(__file__), '../source/onnx-metadata.json')
-    with open(json_file, 'r', encoding='utf-8') as file:
-        content = file.read()
-        items = json.loads(content)
-        items = list(filter(lambda item: item['module'] == "com.microsoft", items))
-        json_root = json_root + items
-    with open(json_file, 'w', encoding='utf-8') as file:
-        file.write(json.dumps(json_root, indent=2))
-
-def _infer():
-    import onnx # pylint: disable=import-outside-toplevel
-    import onnx.shape_inference # pylint: disable=import-outside-toplevel
-    file = sys.argv[2]
-    base = os.path.splitext(file)[0]
-    onnx_model = onnx.load(base + '.onnx')
-    onnx_model = onnx.shape_inference.infer_shapes(onnx_model)
-    onnx.save(onnx_model, base + '.shape.onnx')
+    root_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+    json_file = os.path.join(root_dir, 'source', 'onnx-metadata.json')
+    content = _read(json_file)
+    items = json.loads(content)
+    items = list(filter(lambda item: item['module'] == "com.microsoft", items))
+    json_root = json_root + items
+    _write(json_file, json.dumps(json_root, indent=2))
 
 def main(): # pylint: disable=missing-function-docstring
-    table = { 'metadata': _metadata, 'infer': _infer }
+    table = { 'metadata': _metadata }
     for command in sys.argv[1:]:
         table[command]()
-
 
 if __name__ == '__main__':
     main()
