ed1f74c7741aecdfdf21e7b0d47c6fcd479c91dd
Lutz Roeder <lutzroeder@users.noreply.github.com>
2022-09-24 10:42:26 -0700

    Add .mge extension (#607) (#956)

--- source/app.js
+++ source/app.js
@@ -173,7 +173,7 @@ class Application {
                     'meta',
                     'tflite', 'lite', 'tfl', 'circle',
                     'armnn', 'mnn', 'nn', 'uff', 'uff.txt', 'rknn', 'xmodel', 'kmodel',
-                    'ncnn', 'param', 'tnnproto', 'tm', 'tmfile', 'ms', 'om',
+                    'ncnn', 'param', 'tnnproto', 'tm', 'mge', 'tmfile', 'ms', 'om',
                     'pt', 'pth', 'ptl', 't7',
                     'pkl', 'joblib',
                     'pbtxt', 'prototxt',
