528859ff7cd9d789b04e1ad18554f6e108792907
Lutz Roeder <lutzroeder@users.noreply.github.com>
2022-10-24 02:15:52 -0700

    Update server.js

--- source/view.js
+++ source/view.js
@@ -1570,7 +1570,7 @@ view.ModelFactoryService = class {
         this._host = host;
         this._extensions = new Set([ '.zip', '.tar', '.tar.gz', '.tgz', '.gz' ]);
         this._factories = [];
-        this.register('./message', [ '.netron']);
+        this.register('./server', [ '.netron']);
         this.register('./pytorch', [ '.pt', '.pth', '.ptl', '.pt1', '.pyt', '.pyth', '.pkl', '.pickle', '.h5', '.t7', '.model', '.dms', '.tar', '.ckpt', '.chkpt', '.tckpt', '.bin', '.pb', '.zip', '.nn', '.torchmodel', '.torchscript', '.pytorch', '.ot', '.params', '.trt' ], [ '.model' ]);
         this.register('./onnx', [ '.onnx', '.onn', '.pb', '.onnxtxt', '.pbtxt', '.prototxt', '.txt', '.model', '.pt', '.pth', '.pkl', '.ort', '.ort.onnx', 'onnxmodel' ]);
         this.register('./mxnet', [ '.json', '.params' ], [ '.mar'] );
