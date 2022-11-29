b3862c1268fa51e5db4e3bb83f50fb21ea1230fb
Lutz Roeder <lutzroeder@users.noreply.github.com>
2022-11-28 19:38:05 -0800

    oneDNN support (#1006) (#1005)

--- source/view.js
+++ source/view.js
@@ -1623,6 +1623,7 @@ view.ModelFactoryService = class {
         this.register('./hickle', [ '.h5', '.hkl' ]);
         this.register('./nnef', [ '.nnef', '.dat' ]);
         this.register('./cambricon', [ '.cambricon' ]);
+        this.register('./onednn', [ '.json'] );
     }
 
     register(id, factories, containers) {
