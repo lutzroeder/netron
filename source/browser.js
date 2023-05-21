74a71aaba73e4ab0c725a369413fedd36e169eaf
Lutz Roeder <lutzroeder@users.noreply.github.com>
2023-05-20 18:30:24 -0700

    Update browser.js

--- source/browser.js
+++ source/browser.js
@@ -725,39 +725,34 @@ host.BrowserHost.Context = class {
 if (!('scrollBehavior' in window.document.documentElement.style)) {
     const __scrollTo__ = Element.prototype.scrollTo;
     Element.prototype.scrollTo = function(options) {
-        if (options === undefined) {
-            return;
-        }
-        if (options === null || typeof options !== 'object' || options.behavior === undefined || arguments[0].behavior === 'auto' || options.behavior === 'instant') {
-            if (__scrollTo__) {
-                __scrollTo__.apply(this, arguments);
+        if (options !== undefined) {
+            if (options === null || typeof options !== 'object' || options.behavior === undefined || arguments[0].behavior === 'auto' || options.behavior === 'instant') {
+                if (__scrollTo__) {
+                    __scrollTo__.apply(this, arguments);
+                }
+            } else {
+                const now = () =>  window.performance && window.performance.now ? window.performance.now() : Date.now();
+                const ease = (k) => 0.5 * (1 - Math.cos(Math.PI * k));
+                const step = (context) => {
+                    const value = ease(Math.min((now() - context.startTime) / 468, 1));
+                    const x = context.startX + (context.x - context.startX) * value;
+                    const y = context.startY + (context.y - context.startY) * value;
+                    context.element.scrollLeft = x;
+                    context.element.scrollTop = y;
+                    if (x !== context.x || y !== context.y) {
+                        window.requestAnimationFrame(step.bind(window, context));
+                    }
+                };
+                const context = {
+                    element: this,
+                    x: typeof options.left === 'undefined' ? this.scrollLeft : ~~options.left,
+                    y: typeof options.top === 'undefined' ? this.scrollTop : ~~options.top,
+                    startX: this.scrollLeft,
+                    startY: this.scrollTop,
+                    startTime: now()
+                };
+                step(context);
             }
-            return;
         }
-        const now = () => {
-            return window.performance && window.performance.now ? window.performance.now() : Date.now();
-        };
-        const ease = (k) => {
-            return 0.5 * (1 - Math.cos(Math.PI * k));
-        };
-        const step = (context) => {
-            const value = ease(Math.min((now() - context.startTime) / 468, 1));
-            const x = context.startX + (context.x - context.startX) * value;
-            const y = context.startY + (context.y - context.startY) * value;
-            context.element.scrollLeft = x;
-            context.element.scrollTop = y;
-            if (x !== context.x || y !== context.y) {
-                window.requestAnimationFrame(step.bind(window, context));
-            }
-        };
-        const context = {
-            element: this,
-            x: typeof options.left === 'undefined' ? this.scrollLeft : ~~options.left,
-            y: typeof options.top === 'undefined' ? this.scrollTop : ~~options.top,
-            startX: this.scrollLeft,
-            startY: this.scrollTop,
-            startTime: now()
-        };
-        step(context);
     };
 }
