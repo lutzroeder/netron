915ebf31e78f056678666de7d01e159ddb8959aa
Lutz Roeder <lutzroeder@users.noreply.github.com>
2023-06-13 20:05:38 -0700

    Update notarize.js

--- publish/notarize.js
+++ publish/notarize.js
@@ -6,10 +6,12 @@ exports.default = function (context) {
         const config = context.packager.info.options.config;
         if (process.env.CSC_IDENTITY_AUTO_DISCOVERY !== 'false' && (!config || !config.mac || config.mac.identity !== null)) {
             return notarize.notarize({
+                tool: 'notarytool',
                 appBundleId: context.packager.info.config.appId,
                 appPath: context.appOutDir + '/' + context.packager.appInfo.productFilename + '.app',
-                appleApiKey: process.env.API_KEY_ID,
-                appleApiIssuer: process.env.API_KEY_ISSUER_ID
+                appleApiKey: '~/.private_keys/AuthKey_' + process.env.APPLE_API_KEY_ID + '.p8',
+                appleApiKeyId: process.env.APPLE_API_KEY_ID,
+                appleApiIssuer: process.env.APPLE_API_KEY_ISSUER_ID
             });
         }
     }
