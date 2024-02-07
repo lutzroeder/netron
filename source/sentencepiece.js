8c5ba0766ef5daf55176be8cd0992da5c94f546a
Lutz Roeder <lutzroeder@users.noreply.github.com>
2024-02-06 21:51:11 -0800

    Add SentencePiece test file (#748)

--- source/sentencepiece.js
+++ source/sentencepiece.js
@@ -12,8 +12,8 @@ sentencepiece.ModelFactory = class {
             if (model &&
                 model['1'] && model['1']['1'] === 2 && model['1']['2'] === 5 && model['1']['3'] === 0 &&
                 model['2'] && model['2']['1'] === 2 && model['2']['2'] === 2 && model['2']['3'] === 0 &&
-                model['2']['4'] === 0 && model['2']['10'] === 5 && model['2']['16'] === 0 &&
-                model['2']['40'] === 0 && model['2']['41'] === 0 && model['2']['42'] === 0 && model['2']['43'] === 0) {
+                model['2']['4'] === 0 && model['2']['10'] === 5 &&
+                model['2']['40'] === 0 && model['2']['41'] === 0 && model['2']['42'] === 0) {
                 context.type = 'sentencepiece';
             }
         }
