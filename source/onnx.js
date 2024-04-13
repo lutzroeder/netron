e843c6773cc52b9000ab603939d7037707b9b05b
Lutz Roeder <lutzroeder@users.noreply.github.com>
2024-04-12 18:02:50 -0700

    Update onnx.js

--- source/onnx.js
+++ source/onnx.js
@@ -1407,7 +1407,7 @@ onnx.ProtoReader = class {
         const stream = context.stream;
         if (stream && stream.length > 5) {
             const buffer = stream.peek(Math.min(stream.length, 32));
-            if (buffer[0] === 0x08 && buffer[1] < 0x0A && buffer[2] === 0x12) {
+            if (buffer[0] === 0x08 && buffer[1] < 0x0B && buffer[2] === 0x12) {
                 const producers = [
                     'backend-test', 'BrainwaveCompiler',
                     'CNTK', 'customvision', 'cvflowbackend',
