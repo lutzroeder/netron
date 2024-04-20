a4ccfc30459c7627e0b31217707c43e3d421eed7
Lutz Roeder <lutzroeder@users.noreply.github.com>
2024-04-20 13:02:03 -0700

    Update browser.js

--- source/browser.js
+++ source/browser.js
@@ -129,7 +129,7 @@ host.BrowserHost = class {
                     return obj;
                 });
             };
-            const capabilities = filter(['fetch', 'DataView.prototype.getBigInt64', 'Worker']);
+            const capabilities = filter(['fetch', 'DataView.prototype.getBigInt64', 'Worker', 'Array.prototype.flat']);
             this.event('browser_open', {
                 browser_capabilities: capabilities.map((capability) => capability.split('.').pop()).join(',')
             });
