3ec8fdf557c2a03c67951e9af75caf19e660986e
Lutz Roeder <lutzroeder@users.noreply.github.com>
2025-01-30 20:32:55 -0800

    Update om.js (#1426)

--- source/om.js
+++ source/om.js
@@ -29,6 +29,7 @@ om.Model = class {
 
     constructor(metadata, target) {
         this.format = target.format;
+        this.version = target.signature === 'PICO' ? target.model.version : '';
         const context = {
             metadata,
             signature: target.signature,
@@ -535,6 +536,7 @@ svp.ModelDef = class ModelDef {
         this.graph = [];
         this.name = reader.find(0x800D, 'string');
         this.batch_num = reader.find(0x600A);
+        this.version = '';
         while (reader.position < reader.length) {
             const tag = reader.uint16();
             const value = reader.value(tag);
@@ -606,6 +608,7 @@ svp.ModelDef = class ModelDef {
                 this.graph[0].op = this.graph[0].op.concat(this.graph[i].op);
             }
         }
+        this.version = this.graph[0].op.length === 0 ? 'release' : 'debug';
     }
 };
 
