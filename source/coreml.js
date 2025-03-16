738bb18c8509b8e07b6d6ffbff117ecb617deaf2
Lutz Roeder <lutzroeder@users.noreply.github.com>
2025-03-16 08:00:31 -0700

    Update coreml.js

--- source/coreml.js
+++ source/coreml.js
@@ -1296,7 +1296,7 @@ coreml.Context.Graph = class {
                                     break;
                                 }
                                 case 'float16':
-                                case 'int1': case 'int2': case 'int3': case 'int4': case 'int6': case 'int8':
+                                case 'int1': case 'int2': case 'int3': case 'int4': case 'int6': case 'int8': case 'int32':
                                 case 'uint1': case 'uint2': case 'uint3': case 'uint4': case 'uint6': case 'uint8': {
                                     data = stream.read(size);
                                     break;
