7e6b5657670fc4b8285dd529cf3d2ac3d89dd07b
Lutz Roeder <lutzroeder@users.noreply.github.com>
2025-03-27 22:50:27 -0700

    Update app.js

--- source/app.js
+++ source/app.js
@@ -5,6 +5,7 @@ import * as fs from 'fs';
 import * as os from 'os';
 import * as path from 'path';
 import * as process from 'process';
+import * as updater from 'electron-updater';
 import * as url from 'url';
 
 const app = {};
@@ -292,7 +293,6 @@ app.Application = class {
         if (!electron.app.isPackaged) {
             return;
         }
-        const updater = await import('electron-updater');
         const autoUpdater = updater.default.autoUpdater;
         if (autoUpdater.app && autoUpdater.app.appUpdateConfigPath && !fs.existsSync(autoUpdater.app.appUpdateConfigPath)) {
             return;
