c9c15ef151d71eadec699a88a7d46abdf2a8515d
Jeson Chen <chen.jeson@bytedance.com>
2026-08-28 23:39:12 +0800

    Add node neighborhood highlighting

--- source/grapher.js
+++ source/grapher.js
@@ -161,6 +161,8 @@ grapher.Graph = class {
             }
         });
         edgePathGroupDefs.appendChild(marker("arrowhead"));
+        edgePathGroupDefs.appendChild(marker("arrowhead-input-highlight"));
+        edgePathGroupDefs.appendChild(marker("arrowhead-output-highlight"));
         edgePathGroupDefs.appendChild(marker("arrowhead-select"));
         edgePathGroupDefs.appendChild(marker("arrowhead-hover"));
         for (const nodeId of this.nodes.keys()) {
